5db5da3e74bf593a5f3a2604f2554f0bf7fc869a
daaniakhalith <daania.khalith@gmail.com>
2024-10-24 21:46:07 +0400

    save

---
+++ Tipsy/Controller/CalculateViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    @IBOutlet weak var rwenty: UIButton!
+    
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var ten: UIButton!
+    
+    @IBOutlet weak var zero: UIButton!
+    @IBOutlet weak var splitLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ Tipsy/Controller/ResultViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Khalith on 24/10/2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
