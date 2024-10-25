c230f0d01f13209bb3f03a4e85115573140d4a50
daaniakhalith <daania.khalith@gmail.com>
2024-10-25 15:55:02 +0400

    completed-tipsy

--- Tipsy/Controller/CalculateViewController.swift
+++ Tipsy/Controller/CalculateViewController.swift
@@ -7,7 +7,11 @@
 //
 
 import UIKit
-
+var tip = 0.10
+var billtotal = 0.0
+var billamount = 0.0
+var numberOfPeople = 2
+var final = "0.0"
 class CalculateViewController: UIViewController {
     @IBOutlet weak var rwenty: UIButton!
     
@@ -24,11 +28,27 @@ class CalculateViewController: UIViewController {
     }
         
     @IBAction func calculatePressed(_ sender: UIButton) {
-        
-        
+        let bill = billTextField.text!
+        if bill != "" {
+            billtotal=Double(bill)!
+            billamount =  billtotal * (1 + tip) / Double(numberOfPeople)
+            final = String(format: "%.0f",billamount)
+            //print(billamount)
+            
+        }
+        self.performSegue(withIdentifier: "GoToResult", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "GoToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.result = final
+            destinationVC.tips = Int (tip * 100)
+            destinationVC.peopleCount = numberOfPeople
+        }
     }
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        splitLabel.text = String(Int(sender.value))
+        splitLabel.text = String(format: "%.0f",sender.value)
+        numberOfPeople = Int(sender.value)
     }
     
     @IBAction func tipChanged(_ sender: UIButton) {
@@ -37,7 +57,13 @@ class CalculateViewController: UIViewController {
         zero.isSelected = false
         ten.isSelected = false
         sender.isSelected = true
-        
+        let tipselected = sender.currentTitle!
+        //print(type(of: tipselected))
+        let lastpercentage = String(tipselected.dropLast())
+        let tipnumber = Double(lastpercentage)!
+        //tip = sender.
+        //print(tipnumber)
+        tip = tipnumber/100
     }
     
 }
--- Tipsy/Controller/ResultViewController.swift
+++ Tipsy/Controller/ResultViewController.swift
@@ -6,3 +6,23 @@
 //  Copyright © 2024 The App Brewery. All rights reserved.
 //
 
+import UIKit
+class ResultViewController:
+    UIViewController {
+    var result = "0.0"
+    var peopleCount = 2
+    var tips = 0
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var billAmount: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        billAmount.text = result
+        label.text = "\(peopleCount) people with \(tips)% tips"
+    }
+    
+    @IBAction func recalculate(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+}
+
